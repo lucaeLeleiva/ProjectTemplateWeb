8fe08e5d06c79169975b4cca00317bff034cde6f
lucaeLeleiva <lucaleiva248@gmail.com>
2016-07-21 12:13:40 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -3,14 +3,14 @@ app.config(function ($routeProvider) {
     $routeProvider 
         .when('/', { 
             controller: 'HomeController', 
-            templateUrl: '/views/home.html' 
+            templateUrl: '../views/home.html' 
         })
         .when('/articles/:id',{
       	    controller: 'ArticleController',
-            templateUrl: '/views/article.html'
+            templateUrl: '../views/article.html'
       
         })
         .otherwise({ 
-          redirectTo: '/' 
+          redirectTo: '../' 
         }); 
 });
