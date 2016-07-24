33095e6981d6d921c0af34e3dd0e58836955df07
lucaeLeleiva <lucaleiva248@gmail.com>
2016-07-24 20:27:26 -0300

    Update HomeController.js

--- js/controllers/HomeController.js
+++ js/controllers/HomeController.js
@@ -1,7 +1,7 @@
 app.controller('HomeController', ['$scope', 'articles', 'owner', function($scope, articles, owner) {
   articles.success(function(data) {
     $scope.articles = data;
-  });
+  }),
   owner.success(function(data) {
     $scope.owner = data;
   });
