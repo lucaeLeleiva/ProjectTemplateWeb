559677e68db0674cfed78487ee4f792e8d0bc376
lucaeLeleiva <lucaleiva248@gmail.com>
2016-07-25 10:19:34 -0300

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,15 +1,16 @@
 var app = angular.module('App-Generica', ['ngRoute']);
-    app.config(function ($routeProvider) { 
-        $routeProvider
-            .when('/', { 
-                controller: 'HomeController', 
-                templateUrl: '../ProjectTemplateWeb/views/home.html' 
-            })
-            .when('/articles/:id',{
-        	    controller: 'ArticleController',
-                templateUrl: '../ProjectTemplateWeb/views/article.html'
-            })
-            .otherwise({ 
-                redirectTo: '/'
-            }); 
-    });
+app.config(function ($routeProvider) { 
+    $routeProvider 
+        .when('/', { 
+            controller: 'HomeController', 
+            templateUrl: '../ProjectTemplateWeb/views/home.html' 
+        })
+        .when('/articles/:id',{
+      	    controller: 'ArticleController',
+            templateUrl: '../ProjectTemplateWeb/views/article.html'
+      
+        })
+        .otherwise({ 
+          redirectTo: '/'
+        }); 
+});
