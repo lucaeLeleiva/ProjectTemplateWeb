9f6825a6b75e76c2b56496ce904797b7e5033c91
lucaeLeleiva <lucaleiva248@gmail.com>
2016-07-25 10:26:32 -0300

    Update OwnerController.js

--- js/controllers/OwnerController.js
+++ js/controllers/OwnerController.js
@@ -1,5 +1,5 @@
 app.controller('OwnerController', ['$scope', 'owners', function($scope, owners) {
   owners.success(function(data) {
-    $scope.duenio = data;
+    $scope.owner = data;
   });
 }]);
